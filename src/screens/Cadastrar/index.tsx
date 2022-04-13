f0f1c1c65b76f452fb3c4cfc8f56f94ebab36b94
Ketcia <leticiarafael98@gmail.com>
2022-04-13 08:43:05 -0300

    13/04

--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -6,24 +6,27 @@ import {
   TextInput,
   ImageBackground,
 } from "react-native";
-import { MaterialIcons, Entypo } from "@expo/vector-icons";
+import { MaterialIcons, Entypo, Ionicons } from "@expo/vector-icons";
 import Button from "../../components/Button";
 import styles from "./styles";
 import { LoginTypes } from "../../types/Screen.types";
 
-export default function Login({ navigation }: LoginTypes) {
+export default function Cadastrar({ navigation }: LoginTypes) {
   async function handleSignIn() {
-    console.log("Login");
+    console.log("Cadastrar");
   }
-  function handleCadastrar() {
-    navigation.navigate("Cadastrar");
+  function handleLogin() {
+    navigation.navigate("Login");
   }
 
   return (
     <View style={styles.container}>
-
         <KeyboardAvoidingView>
-          <Text style={styles.title}>Login</Text>
+          <Text style={styles.title}>Cadastre-se</Text>
+          <View style={styles.formRow}>
+            <Ionicons name="person" style={styles.icon} />
+            <TextInput style={styles.input} placeholder="Nome" />
+          </View>
           <View style={styles.formRow}>
             <MaterialIcons name="email" style={styles.icon} />
             <TextInput
@@ -42,8 +45,8 @@ export default function Login({ navigation }: LoginTypes) {
               autoCapitalize="none"
             />
           </View>
-          <Button title="Login" type="cinza" onPress={handleSignIn} />
-          <Button title="Cadastre-se" type="cinza" onPress={handleCadastrar} />
+          <Button title="Salvar" type="cinza" onPress={handleSignIn} />
+          <Button title="Voltar" type="cinza" onPress={handleLogin} />
         </KeyboardAvoidingView>
     </View>
   );
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.preto
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.black,
+    marginBottom: 20,
+  },
+  formRow: {
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 5,
+    color:colors.branco
+  },
+  label: {
+    fontSize: 18,
+    color: colors.black,
+    padding: 5
+  },
+  input: {
+    fontSize: 18,
+    padding: 5,
+    width: "80%",
+    
+  },
+  icon: {
+    fontSize: 24,
+    color: colors.brancoEscuro,
+    padding: 10
+  }
+})
+
+export default styles;
