b2416f69d0922e336877809f20cc50b2091845e4
Ketcia <leticiarafael98@gmail.com>
2022-06-21 09:15:08 -0300

    build

--- src/services/data/api.ts
+++ src/services/data/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-
 const api = axios.create({
     baseURL: "https://laravelweb2022.herokuapp.com/api",
-    headers:{
+    headers: {
         Accept: 'application/json',
     }
 })
+export default api;
